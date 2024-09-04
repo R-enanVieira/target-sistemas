d87b11a222221364b85f3e4529e7fe96a3007184
R-enanVieira <renanv.guedes7@gmail.com>
2024-09-03 23:55:29 -0300

    adicionando a solução da questão 3

---
+++ 3.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int indice = 12, soma = 0, k = 1;
+
+  while (k < indice) {
+    k += 1;
+    soma += k;
+  }
+
+  cout << soma << "\n";
+}
